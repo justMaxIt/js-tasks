e5fdf6cfc0afcf43818852b3363a747a690c68b4
justMaxIt <maximfedor2015@gmail.com>
2020-07-27 16:32:17 +0300

    function number Fibanacci

---
+++ js/hw3consFibban.js
@@ -0,0 +1,20 @@
+/* Создайте функцию, которая будет выводить следующее число фибаначчи(сумму двух предыдущих чисел в ряду) при каждом ее вызове:
+newFib() // 0
+newFib() // 1
+newFib() // 1
+newFib() // 2
+newFib() // 3
+newFib() // 5
+newFib() // 8
+... */
+
+// function fib(a) {
+//     if (a[0] === undefined) {
+//         a.push(0)
+//     }
+//     if (a[0] === 0 && a.length === 1) {
+//         a.push(1)
+//     }
+//     else { a.push(a.reduce((accum, value) => value + a[a.length - 2])) }
+//     console.log(a[a.length - 1])
+// }

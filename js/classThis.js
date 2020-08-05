dd63ff46610a5c7c4cc4b2a8b44f8f1c7cdea576
justMaxIt <maximfedor2015@gmail.com>
2020-08-05 17:47:04 +0300

    class this Cat Dog Animal

---
+++ js/classThis.js
@@ -0,0 +1,66 @@
+// Задание class:
+// Создать класс Animal
+// со свойствами:
+// - вес
+//     - возраст
+
+// методом:
+// - голос // функция вызвав которую ваше животное подаст голос: кот - мяу, собака - гав
+
+// Создать новый класс Dog наследующий свойства и методы у класса Animal
+
+// и добавить ему собственный метод:
+// - рычать
+
+// class Animal {
+//     constructor(name) {
+//         this.name = name;
+//     }
+
+//     speak() {
+//         console.log(this.name + ' издает звук.');
+//     }
+// }
+
+// class Dog extends Animal {
+//     speak() {
+//         console.log(this.name + ' лает.');
+//     }
+// }
+
+// ЛенаМейнАкад
+// Кий, я сбросила решение в зум
+
+// Kiy Andreev 【 ๏̯͡๏】
+// class Animal {
+//     constructor(name, weight, age) {
+//         this.name = name;
+//         this.weight = weight;
+//         this.age = age;
+//     }
+
+//     speak() {
+//         console.log(this.name + ' издает звук.');
+//     }
+// }
+
+// class Dog extends Animal {
+//     speak() {
+//         console.log(this.name + ' лает.');
+//     }
+//     rrr() {
+//         console.log(this.name + ' rrrrrrr.');
+//     }
+// }
+
+// class Cat extends Animal {
+//     speak() {
+//         console.log(this.name + ' мяу.');
+//     }
+// }
+
+// let Barsik = new Cat("Barsik", 15, 8)
+// Barsik.speak()
+
+// let Bobik = new Dog("Bobik", 25, 5)
+// Bobik.rrr()
